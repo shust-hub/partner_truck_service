e561c302b8790f3e8770f62e73c64256cd5ad507
shust-hub <alyonashustov@yandex.ru>
2019-07-23 20:22:17 +0300

    Верстка сайта Partner Truck Service

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('.slider').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  infinite: true,
+	  speed: 350,
+	  adaptiveHeight: true,
+	  // autoplay: true,
+  	  autoplaySpeed: 3000,
+	  arrows: false,
+      responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+	});
+	$(".slider-nav a").click(function(e){
+        e.preventDefault();
+        slideIndex = $(this).index();
+        $( '.slider' ).slick('slickGoTo', slideIndex, true);
+    });
+});
